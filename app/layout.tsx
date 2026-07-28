11765f5da2a6428060d60da55bdb23517f45f45a
yadaw-hash <tesfaytewelde2015@gmail.com>
2026-07-28 10:09:38 +0300

    setting up prisma and shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Geist } from 'next/font/google';
 import './globals.css';
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({
   subsets: ['latin'],
@@ -18,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className={cn("font-sans", geist.variable)}>
       <body className={inter.className}>{children}</body>
     </html>
   );
